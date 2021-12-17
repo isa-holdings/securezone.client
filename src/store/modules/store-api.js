2ec04206aa090c216a35191f03083ed2c71690cd
Charl Cronje <charlcp@gmail.com>
2021-12-17 13:35:37 +0200

    All

--- src/store/modules/store-api.js
+++ src/store/modules/store-api.js
@@ -2,7 +2,7 @@ import { reactive } from 'vue'
 
 const state = reactive({
     API_TOKEN: null,
-    URL: process.env.GRAPHQL_URI || 'http://api.securezone.co.za/test/gql' // Change to your graphql endpoint.
+    URL: process.env.GRAPHQL_URI || 'http://api.securezone.co.za/gql' // Change to your graphql endpoint.
 });
 
 export default {
